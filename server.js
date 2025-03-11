133b461be87ba0ac00225d259a48752854df5c1a
umvelhonoob <carlossandressantos01@gmail.com>
2025-03-11 20:44:28 -0300

    server.js

--- server.js
+++ server.js
@@ -221,7 +221,7 @@ io.on('connection', (socket) => {
 });
 
 // Iniciar o servidor
-server.listen(PORT, (10000) => {
+server.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
     console.log("Modo:", process.env.NODE_ENV || "development");
     console.log("WebSocket Config:", {
